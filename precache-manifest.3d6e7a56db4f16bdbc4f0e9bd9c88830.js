b69084e55241b5cf66e3a20a4a159be3b8abdb8a
AhmedELShafaie <ahmed.fathy@gmail.com>
2020-09-25 19:20:53 +0000

    Updates

---
+++ precache-manifest.3d6e7a56db4f16bdbc4f0e9bd9c88830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01000a9bb2b84153c0d5affee0b3cd00",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "1efe34ac206d8bebc9c0",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "1efe34ac206d8bebc9c0",
+    "url": "/calculator/static/js/main.027dc7db.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calculator/precache-manifest.76987964285b3e756c6e8a7f63ad8765.js"
+  "/calculator/precache-manifest.3d6e7a56db4f16bdbc4f0e9bd9c88830.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculator/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
